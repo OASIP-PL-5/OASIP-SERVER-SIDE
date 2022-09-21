993f9571986416875509d7a0a3194e201b45ce82
Joe Pattarapon <72536700+Joe-sit@users.noreply.github.com>
2022-09-21 23:26:34 +0700

    implement specific authentication to only users endpoint

--- src/main/java/sit/int221/oasipservice/SecurityConfigurer.java
+++ src/main/java/sit/int221/oasipservice/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package sit.int221.oasipservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import sit.int221.oasipservice.exceptions.CustomAuthenticationFailureHandler;
 import sit.int221.oasipservice.filters.JwtFilter;
 import sit.int221.oasipservice.services.UserService;
 
@@ -29,10 +32,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     //    PasswordEncoder
 //    NoOpPasswordEncoder.getInstance()
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder();
-    }
 
     @Autowired
     private JwtFilter jwtFilter;
@@ -43,13 +42,46 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+//    ใช้ try-catch ดักได้
+//    @Bean
+//    public AuthenticationFailureHandler authenticationFailureHandler(){
+//        return new CustomAuthenticationFailureHandler();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/api/login").permitAll()
+                .authorizeRequests()
+                //public endpoints
+                .antMatchers("/api/login").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/event-categories/**").permitAll()
+                .antMatchers("/api/events/**").permitAll()
+                .antMatchers("/api/match").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/users").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/events/{id}").permitAll()
+
+
+
+                //privilege endpoint
+//                .antMatchers("/api/**").hasAnyRole(String.valueOf(EnumRole.admin))
+//                .antMatchers(HttpMethod.POST,"/api/event-categories/**").hasAnyRole(String.valueOf(EnumRole.admin))
+//                .antMatchers(HttpMethod.PUT,"/api/event-categories/**").hasAnyRole(String.valueOf(EnumRole.admin))
+                .antMatchers("/api/**").hasRole("admin")
+//                .antMatchers(HttpMethod.POST,"/api/users/**").hasAnyRole("ADMIN")
+//                .antMatchers(HttpMethod.PUT,"/api/users/**").hasAnyRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE,"/api/users/**").hasAnyRole("ADMIN")
+
+
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+
     }
 }
