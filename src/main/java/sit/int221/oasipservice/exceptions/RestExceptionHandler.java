a6e13ee2da454f00039faf5287994dbeddfa4864
Joe Pattarapon <72536700+Joe-sit@users.noreply.github.com>
2022-10-18 12:57:45 +0700

    [+update] File uploading

---
+++ src/main/java/sit/int221/oasipservice/exceptions/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package sit.int221.oasipservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import sit.int221.oasipservice.models.UploadResponseMessage;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<UploadResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                .body(new UploadResponseMessage("Unable to upload. File is too large!"));
+    }
+}
